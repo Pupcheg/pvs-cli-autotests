46b2839a2bd30f686c1041a383cb279e31172cf4
Dasha <romanova5dada@gmail.com>
2026-07-03 16:48:17 +0300

    update main.py
    Add required command line argument "analyzer_path".
    Add optional "--report" argument for report output path.

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from src.run_tests import run_tests
+import argparse
+import os
 
-if __name__ == "__main__":  # пример работы
-    stdout, stderr, code = run_tests(
-        "C:\\Program Files (x86)\\PVS-Studio\\pvs-js\\pvs-js.exe")
-    print(stdout, stderr, code)
+if __name__ == "__main__":  # пример работы. используйте только анлийские буквы в путях 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("analyzer_path", help="Path to the executable file (*.exe).")
+    parser.add_argument("--report",default="reports", help="Path to save the report (*.md).")# пока не работает. Жду когда Родион поправит код для отчетов
+    args=parser.parse_args()
+    if os.path.exists(args.analyzer_path) and os.path.isfile(args.analyzer_path):
+        stdout, stderr, code = run_tests(args.analyzer_path)
+        print(stdout, stderr, code)
+    else:
+        print("Рath invalid.")
+    
  
