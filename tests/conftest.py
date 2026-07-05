0e6bab2d6fce939c1a047e1446529a2a21f30925
RodionYakushin <rodionyakushin@gmail.com>
2026-07-05 17:28:31 +0300

    Update analyzer fixture to include command_line parameter

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,11 @@
 import pytest
-#import sys
 import os
-#from pathlib import Path
-#sys.path.insert(0, str(Path(__file__).parent.parent / "src"))
 from run_static_analyzer import run_static_analyzer
 
 @pytest.fixture
 def analyzer():
     analyzer_path = os.environ.get("STATIC_ANALYZER_PATH")
 
-    def _run(source_file_path, expected_code=None):
-        return run_static_analyzer(analyzer_path, source_file_path, expected_code=expected_code)
+    def _run(source_file_path, command_line=None, expected_code=None):
+        return run_static_analyzer(analyzer_path, source_file_path, command_line=command_line, expected_code=expected_code)
     return _run
