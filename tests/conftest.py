5b81c6e92273f170437fcc116ff3ad352bf50471
RodionYakushin <rodionyakushin@gmail.com>
2026-07-05 09:52:48 +0300

    Modify analyzer fixture to include expected_code
    Updated the analyzer fixture to accept an optional expected_code parameter.

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,6 @@ from run_static_analyzer import run_static_analyzer
 def analyzer():
     analyzer_path = os.environ.get("STATIC_ANALYZER_PATH")
 
-    def _run(source_file_path):
-        return run_static_analyzer(analyzer_path, source_file_path)
+    def _run(source_file_path, expected_code=None):
+        return run_static_analyzer(analyzer_path, source_file_path, expected_code=expected_code)
     return _run
