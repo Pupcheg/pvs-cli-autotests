dd025f84a6293d26faf9bfdefe226f6ed969cb0b
Dasha <romanova5dada@gmail.com>
2026-07-07 16:12:46 +0300

    old remove json

--- src/remove_json_reports.py
+++ src/remove_json_reports.py
@@ -1,10 +1,22 @@
 from pathlib import Path
 def remove_json_reports():
-    root_dir=Path(__file__).parent.parent
-    json_files = list(root_dir.rglob("*.json"))
-
-    if not json_files:
+    reports_dir = Path("reports")
+    if not reports_dir.exists():
         return
     else:
-        for file in json_files:
+        for file in reports_dir.glob("*.json"):
+            file.unlink()
+
+    reports_dir = Path("tests/test_3_cli_output_no_flag/examples_ts_js/test_example_1")
+    if not reports_dir.exists():
+        return
+    else:
+        for file in reports_dir.glob("*.json"):
+            file.unlink()
+    
+    reports_dir = Path("tests/test_3_cli_output_no_flag/examples_ts_js/test_example_2")
+    if not reports_dir.exists():
+        return
+    else:
+        for file in reports_dir.glob("*.json"):
             file.unlink()
