1f13e770b7172f4b13d227c88470fedc57a32bce
Dasha <romanova5dada@gmail.com>
2026-07-07 10:38:41 +0300

    fix

--- src/tests_tools.py
+++ src/tests_tools.py
@@ -7,13 +7,13 @@ def get_license_path():
     if system == 'Windows':
         return Path(os.getenv('APPDATA'))/"PVS-Studio"/"Settings.xml"
     elif system=='Darwin':
-        return Path.home() / "Library" / "Application Support" / "PVS-Studio" / "Settings.xml"
+        return Path.home() / "Library" / "Application Support" / "PVS-Studio" / "PVS-Studio.lic"
     else:
-        license_original = Path.home() / ".config" / "PVS-Studio" / "Settings.xml"
+        license_original = Path.home() / ".config" / "PVS-Studio" / "PVS-Studio.lic"
         if license_original.exists():
             return license_original
         else:
-            return Path.home() / "PVS-Studio" / "Settings.xml"
+            return Path.home() / "PVS-Studio" / "PVS-Studio.lic"
 
 
 def get_test_dirs():
