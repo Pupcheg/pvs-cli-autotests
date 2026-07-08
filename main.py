38e6a04431292d98b9cabceb458e21b6b5cd2b64
RodionYakushin <rodionyakushin@gmail.com>
2026-07-08 14:19:54 +0300

    Pass pytest stdout/stderr to build_report for parsing test results

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ try:
     from get_analyzer_version import get_analyzer_version
     from remove_json_reports import remove_json_reports
 except ModuleNotFoundError:
-    print("Package not installed. Use ","pip install -e .",".")
+    print("Package not installed. Use pip install -e .")
     exit(1)
 import argparse
 import os
@@ -17,8 +17,8 @@ if __name__ == "__main__":  # используйте только латиниц
         default=None,
         help="Path to save the report.md (optional). Must be a full path to a file, e.g. C:\\reports\\result.md. If not specified, saves to reports/final/report.md"
     )
-    args=parser.parse_args()
-    if os.path.exists(args.analyzer_path) and os.path.isfile(args.analyzer_path) :
+    args = parser.parse_args()
+    if os.path.exists(args.analyzer_path) and os.path.isfile(args.analyzer_path):
     
         stdout, stderr, code = run_tests(args.analyzer_path)
         print(stdout, stderr, code)
@@ -26,10 +26,10 @@ if __name__ == "__main__":  # используйте только латиниц
         build_report(
             reports_dir="reports",
             output_path=args.report,
-            analyzer_version=get_analyzer_version(args.analyzer_path)
+            analyzer_version=get_analyzer_version(args.analyzer_path),
+            stdout=stdout,
+            stderr=stderr
         )
         remove_json_reports()
     else:
-        print("Рath invalid.")
-    
- 
+        print("Path invalid.")
